3b19d98f241222879cc5e6a0df4cc350319e9841
@specktro <specktro@nonull.mx>
2025-02-20 22:42:55 -0600

    feat: Create LogRepositoryImpl and route datasource uses

--- src/infrastructure/datasources/file-system.datasource.ts
+++ src/infrastructure/datasources/file-system.datasource.ts
@@ -1,6 +1,6 @@
 import fs from 'fs'
-import {LogDatasource} from "../../datasources/log.datasource";
-import {LogEntity, LogSeverityLevel} from "../../entities/log.entity";
+import {LogDatasource} from "../../domain/datasources/log.datasource";
+import {LogEntity, LogSeverityLevel} from "../../domain/entities/log.entity";
 
 export class FileSystemDatasource implements LogDatasource {
     private readonly logPath = 'logs/'
