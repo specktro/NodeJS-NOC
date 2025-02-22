8a93379a631ede60f64422d1018039f7472271c5
@specktro <specktro@nonull.mx>
2025-02-22 14:26:52 -0600

    feat: Add file system repository to server class

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,3 +1,6 @@
+import {LogRepository} from "../../repository/log.repository";
+import {LogEntity, LogSeverityLevel} from "../../entities/log.entity";
+
 interface CheckServiceUseCase {
     execute(url: string): Promise<boolean>
 }
@@ -6,7 +9,10 @@ type SuccessCallback = () => void
 type ErrorCallback = (error: string) => void
 
 export class CheckService implements CheckServiceUseCase {
-    constructor(private readonly successCallback: SuccessCallback, private readonly errorCallback: ErrorCallback) {}
+    constructor(
+        private readonly logRepository: LogRepository,
+        private readonly successCallback: SuccessCallback,
+        private readonly errorCallback: ErrorCallback) {}
 
     public async execute(url: string): Promise<boolean> {
         try {
@@ -15,11 +21,14 @@ export class CheckService implements CheckServiceUseCase {
                 throw new Error(`Failed to fetch request ${url}`)
             }
 
+            const log = new LogEntity(`Service ${url} working`, LogSeverityLevel.low)
+            await this.logRepository.saveLog(log)
             this.successCallback()
-            console.log(`${url} is ok`)
             return true
         } catch (error) {
-            this.errorCallback(`${error}`)
+            const errorMessage = `${url} is not ok. ${error}`
+            const log = new LogEntity(errorMessage, LogSeverityLevel.high)
+            await this.logRepository.saveLog(log)
             console.log(error)
             return false
         }
